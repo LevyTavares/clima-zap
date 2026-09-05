676fa7a204f7a9701ab8db8f8a2ceed8d9c883f1
guithunder <g.lopessatanacruz@gmail.com>
2026-09-05 15:21:14 -0300

    feat: adiciona nova rota para obter dados climáticos do Cariri e implementa a função de busca na API do Open-Meteo

---
+++ backend/app/api.py
@@ -0,0 +1,21 @@
+import httpx from app.schemas import WeatcherData
+
+async def fetch_cariri_weather() -> WeatcherData:
+    url = "https://api.open-meteo.com/v1/forecast"
+
+#coordenadas aproximadas do Cariri(Barbalha/juazeiro do Norte)
+    params ={
+        latitiude: -7.31,
+        longitude: -39.31,
+        current_weather: "temperature_2m,relative_humidity_2m,rain,uv_index",
+        
+    }
+    
+    # A requisiçao assicrona 
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, params=params)
+        response.raise_for_status() # o status não for 200 OK
+        data = response.json()
+        
+        # O JSON bruto para o Pydantic validar
+        return WeatherData(**data)
